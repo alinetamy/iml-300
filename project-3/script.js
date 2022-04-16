fe65e626a9294e475b1c6d8c224c61bc370329c0
alinesaruhashi <saruhash@usc.edu>
2022-04-16 10:57:31 -0700

    Added the right files to the folder

--- project-3/script.js
+++ project-3/script.js
@@ -1,15 +1,38 @@
-$(document).ready(function () {   
-    $('#flower').click(function () {
-      $('.text').toggle();
+$(document).ready(function () {
+    $(".myth-container").click(function () {
+        $(this).css("background-image", "linear-gradient(#FFC3F2, #FFFFFF)"),
+            $(this).css("filter", "drop-shadow(8px 8px 10px white)"),
+            $(this).css("border", "0px");
+        //            $(".truth-container-first").hide()
     });
-    
-     $('#plants').click(function () {
-//        $('.infopanel').toggle(2000);     
-        $(this).toggleClass("bigger");
+
+    $("#myth-1").click(function () {
+        $("#truth-1").toggle(),
+            $("#truth-2, #truth-3, #truth-4, #truth-5, #truth-6").hide();
     });
-    
-     $(function () {
-        $("#flower").draggable();
+
+    $("#myth-2").click(function () {
+        $("#truth-2").toggle(),
+        $("#truth-1, #truth-3, #truth-4, #truth-5, #truth-6").hide();
     });
-    
+
+    $("#myth-3").click(function () {
+        $("#truth-3").toggle(),
+        $("#truth-2, #truth-1, #truth-4, #truth-5, #truth-6").hide();
+    });
+
+    $("#myth-4").click(function () {
+        $("#truth-4").toggle(),
+        $("#truth-2, #truth-3, #truth-1, #truth-5, #truth-6").hide();
+    });
+
+    $("#myth-5").click(function () {
+        $("#truth-5").toggle(),
+        $("#truth-2, #truth-3, #truth-4, #truth-1, #truth-6").hide();
+    });
+
+    $("#myth-6").click(function () {
+        $("#truth-6").toggle(),
+        $("#truth-2, #truth-3, #truth-4, #truth-1, #truth-5").hide();
+    });
 });
